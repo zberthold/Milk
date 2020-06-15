71e7ef1f8089411399b4bcc532a4c9c154323a05
Zoe <zberthold@imsa.edu>
2020-06-14 20:22:46 -0500

    add view did appear

--- Milk/ViewController.swift
+++ Milk/ViewController.swift
@@ -15,6 +15,9 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        print(":hey")
+    }
 
 }
 

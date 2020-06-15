888209e157a789f918098149238a22cd84ad8871
Zoe <zberthold@imsa.edu>
2020-06-14 20:03:12 -0500

    Initial Commit

---
+++ Milk/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Milk
+//
+//  Created by Zoe Berthold on 6/14/20.
+//  Copyright © 2020 Zoe Berthold. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
